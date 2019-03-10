8aaf6ebb93ebb741375b84d2244e08c2aabaf539
Neat <neatk13@gmail.com>
2019-03-10 14:24:55 +0700

    Add file to git

---
+++ app/src/main/java/kh/com/bizsolution/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package kh.com.bizsolution.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scroll_view);
+    }
+}
